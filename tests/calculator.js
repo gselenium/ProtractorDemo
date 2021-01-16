51f9821ae6e70a5004f30dc003ceaa6cb348d703
Govardhan <govardhan.selenium@gmail.com>
2021-01-17 02:13:06 +0530

    my first protractor project commit

---
+++ tests/calculator.js
@@ -0,0 +1,32 @@
+let homepage = require('../pages/homepage.js');
+
+describe('demo calculator tests',function(){
+
+    it('addition test', function(){
+       // browser.get("https://juliemr.github.io/protractor-demo/");
+        homepage.get("https://juliemr.github.io/protractor-demo/");
+        //element(by.model('first')).sendKeys('2');
+        homepage.enterFirstNumber('5');
+        //element(by.model('second')).sendKeys('3');
+        homepage.enterSecondNumber('6');
+       // element(by.css('[ng-click="doAddition()"]')).click();
+       homepage.clickGoButton();       
+       browser.sleep(6000);
+       homepage.verifyResult('11');
+
+    });   
+
+    it('Substraction test', function(){
+        // browser.get("https://juliemr.github.io/protractor-demo/");
+         homepage.get("https://juliemr.github.io/protractor-demo/");
+         //element(by.model('first')).sendKeys('2');
+         homepage.enterFirstNumber('5');
+         //element(by.model('second')).sendKeys('3');
+         homepage.enterSecondNumber('6');
+        // element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGoButton1();       
+        browser.sleep(6000);
+        homepage.verifyResult('11');
+ 
+     });   
+});
