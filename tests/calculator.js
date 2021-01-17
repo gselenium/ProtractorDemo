1d0c32848d3aedb5047ab424567e0a71bbdbf241
Govardhan <govardhan.selenium@gmail.com>
2021-01-17 14:16:27 +0530

    Updated as only test in calculator.js file

--- tests/calculator.js
+++ tests/calculator.js
@@ -14,19 +14,5 @@ describe('demo calculator tests',function(){
        browser.sleep(6000);
        homepage.verifyResult('11');
 
-    });   
-
-    it('Substraction test', function(){
-        // browser.get("https://juliemr.github.io/protractor-demo/");
-         homepage.get("https://juliemr.github.io/protractor-demo/");
-         //element(by.model('first')).sendKeys('2');
-         homepage.enterFirstNumber('5');
-         //element(by.model('second')).sendKeys('3');
-         homepage.enterSecondNumber('6');
-        // element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickGoButton1();       
-        browser.sleep(6000);
-        homepage.verifyResult('11');
- 
-     });   
+    }); 
 });
